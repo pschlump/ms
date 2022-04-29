35d1a07f4454e81932eaee8050008eb0c3baf890
Philip Schlump <pschlump@gmail.com>
2022-04-29 03:04:38 -0600

    Added ParseBoolChecked

--- parse_bool.go
+++ parse_bool.go
@@ -1,5 +1,7 @@
 package ms
 
+import "fmt"
+
 var trueValues = map[string]bool{
 	"t":    true,
 	"T":    true,
@@ -15,23 +17,54 @@ var trueValues = map[string]bool{
 	"ON":   true,
 }
 
-/// func init() {
-/// 	trueValues = make(map[string]bool)
-/// 	trueValues["t"] = true
-/// 	trueValues["T"] = true
-/// 	trueValues["yes"] = true
-/// 	trueValues["Yes"] = true
-/// 	trueValues["YES"] = true
-/// 	trueValues["1"] = true
-/// 	trueValues["true"] = true
-/// 	trueValues["True"] = true
-/// 	trueValues["TRUE"] = true
-/// 	trueValues["on"] = true
-/// 	trueValues["On"] = true
-/// 	trueValues["ON"] = true
-/// }
-
 func ParseBool(s string) (b bool) {
 	_, b = trueValues[s]
 	return
+}
+
+var trueValues2 = map[string]bool{
+	"t":    true,
+	"T":    true,
+	"yes":  true,
+	"Yes":  true,
+	"YES":  true,
+	"1":    true,
+	"true": true,
+	"True": true,
+	"TRUE": true,
+	"on":   true,
+	"On":   true,
+	"ON":   true,
+
+	"f":     false,
+	"F":     false,
+	"no":    false,
+	"No":    false,
+	"NO":    false,
+	"0":     false,
+	"false": false,
+	"False": false,
+	"FALSE": false,
+	"off":   false,
+	"Off":   false,
+	"OFF":   false,
+}
+
+func ParseBoolChecked(s string) (b bool, err error) {
+	v, ok := trueValues2[s]
+	if ok {
+		b = v
+		return
+	}
+	err = fmt.Errorf("Invalid Boolean Value")
+	return
+}
+
+func InList(s string, v []string) bool {
+	for _, q := range v {
+		if s == q {
+			return true
+		}
+	}
+	return false
 }
